c5f0dff3a0128aa4f71d443f22d754cba469b015
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-08-13 13:31:42 +0900

    Modify mix.exs

--- mix.exs
+++ mix.exs
@@ -72,9 +72,14 @@ defmodule Rpi4MouseUi.MixProject do
     MIX_ENV:    #{System.get_env("MIX_ENV") || Mix.env()}
     """)
 
-    {_, 0} = System.cmd("mix", ["deps.get"], into: IO.stream(:stdio, :line))
-    {_, 0} = System.cmd("mix", ["deps.compile"], into: IO.stream(:stdio, :line))
-    {_, 0} = System.cmd("mix", ["assets.setup"], into: IO.stream(:stdio, :line))
-    {_, 0} = System.cmd("mix", ["assets.deploy"], into: IO.stream(:stdio, :line))
+    if Mix.target() == :host do
+      {_, 0} = System.cmd("mix", ["assets.setup"], into: IO.stream(:stdio, :line))
+      {_, 0} = System.cmd("mix", ["assets.build"], into: IO.stream(:stdio, :line))
+    else
+      {_, 0} = System.cmd("mix", ["deps.get"], into: IO.stream(:stdio, :line))
+      {_, 0} = System.cmd("mix", ["deps.compile"], into: IO.stream(:stdio, :line))
+      {_, 0} = System.cmd("mix", ["assets.setup"], into: IO.stream(:stdio, :line))
+      {_, 0} = System.cmd("mix", ["assets.deploy"], into: IO.stream(:stdio, :line))
+    end
   end
 end
